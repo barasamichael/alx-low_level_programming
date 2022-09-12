542bb66f15a758c9c32eea29511340965fe819c7
barasamichael <ourdigitaltimes@gmail.com>
2022-09-12 10:21:02 -0700

    Display _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ * This program displays the word _putchar
+ */
+
+#include "_putchar.h"
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return(0);
+}
