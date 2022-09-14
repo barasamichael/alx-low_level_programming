bf76a316af9593f7b114d59fb7e9b617221d0ba6
barasamichael <ourdigitaltimes@gmail.com>
2022-09-14 16:58:47 -0700

    Add return statement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,21 +13,19 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
+	if (n >= 98)
 	{			
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			
 			if (i != 98)
-			{
 				printf(", ");
-			}
 			
 		}
 	
 	} else 
-	{
 		printf("\n");
-	}
+
+	return (0);
 }
