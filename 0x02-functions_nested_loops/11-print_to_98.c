56d0dbf66aee69056044fe2c8003359964588d2a
barasamichael <ourdigitaltimes@gmail.com>
2022-09-14 17:19:29 -0700

    Remove all trailing whitespaces

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,14 @@ void print_to_98(int n)
 	int i;
 
 	if (n >= 98)
-	{			
+	{
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			
 			if (i != 98)
-				printf(", ");
+				printf(", ")
 		}
-	
-	} 
+	}
 	else if(n <= 98)
 	{
 		for (i = n; i <= 98; i++)
