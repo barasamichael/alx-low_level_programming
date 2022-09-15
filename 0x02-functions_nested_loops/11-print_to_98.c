035be6224dbccb1fde22f2316e9720cad43fb094
barasamichael <ourdigitaltimes@gmail.com>
2022-09-14 17:09:19 -0700

    Support Values of n Lesser than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,5 +25,17 @@ void print_to_98(int n)
 		}
 	
 	} 
+	else if(n <= 98)
+	{			
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			
+			if (i != 98)
+				printf(", ");
+			
+		}
+	
+	} 
 	printf("\n");
 }
