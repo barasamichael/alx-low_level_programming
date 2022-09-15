9a6ec4ba774755b2fd09fcdcb597acac3c907603
barasamichael <ourdigitaltimes@gmail.com>
2022-09-15 05:17:38 +0300

    Display Sum of Even Fibonacci Numbers Less Than 400000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,12 +9,10 @@ int main(void)
 	/* Initialize first and second terms */
 	long int term_1 = 1;
 	long int term_2 = 2;
-	long int total = 100;
-	long int count;
 	long int next_term = term_1 + term_2;
 	long int sum;
 
-	for (next_term; next_term > 400000;)
+	while (next_term > 400000)
 	{
 		if (next_term % 2 == 0)
 			sum += next_term;
