d0a513df05e11c97acfcff2334973d4d88729b4f
barasamichael <ourdigitaltimes@gmail.com>
2022-09-15 04:53:34 +0300

    Display First 100 Fibonnacci Numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+	/* Initialize first and second terms */
+	int term_1 = 0;
+	int term_2 = 1;
+	int total = 100;
+
+	printf("%d, %d, ", term_1, term_2);
+
+	int count;
+	int next_term = term_1 + term_2;
+
+	for (count = 3; count <= total; count++)
+	{
+		if (count != total)
+			printf("%d, ", next_term);
+		else
+			printf("%d\n", next_term);
+		term_1 = term_2;
+		term_2 = next_term;
+		next_term = term_1 + term_2;
+	}
+	return (0);
+}
