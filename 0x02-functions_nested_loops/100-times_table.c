4db2799f07a47eadb54d82f7b5da1f8102f5d489
barasamichael <ourdigitaltimes@gmail.com>
2022-09-15 04:13:25 +0300

    Make File Betty Compliant

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,17 +9,21 @@
 void print_times_table(int n)
 {
 	int table_size = n;
+	
 	if (table_size <= 15 && table_size >= 0)
 	{
 		/* Display row */
 		int row;
+		
 		for (row = 0; row <= table_size; row++)
 		{
 			int product; /* holds the product of row and column */
 			int column;
+			
 			for (column = 0; column <= table_size; column++)
 			{
 				product = row * column;
+				
 				if (column != 0)
 					printf("%4d", product);
 				else
