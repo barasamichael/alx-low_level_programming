01330394e4bf4687047aa8be6cd1af56fccf0e5e
barasamichael <ourdigitaltimes@gmail.com>
2022-09-15 04:30:34 +0300

    Display Sum of Multiples of 3 and 5 Below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,25 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * print_times_table - entry point.
  * @n: input.
  * Return: Always 0 (success).
  */
-void print_times_table(int n)
-{
-	return (0);
+
+void main(){
+	int max_value = 1024;
+	int value;
+	int sum = 0; /* Holds sum for all multiples */
+
+	for (value = 1; value < max_value; value++)
+	{
+		/* Find Multiples of 3 */
+		if (value % 3 == 0)
+			sum += value;
+
+		/* Find Multiples of 5 */
+		if (value % 5 == 0)
+			sum += value;
+	}
+
+	printf("%d\n", sum);
 }
