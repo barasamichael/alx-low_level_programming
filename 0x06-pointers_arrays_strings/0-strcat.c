cc378370495751cce2aaf5bb40706868eda7018d
barasamichael <ourdigitaltimes@gmail.com>
2022-09-21 19:29:20 -0700

    Concatenate Two Strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,12 +19,16 @@ char *_strcat(char *dest, char *src)
 	int len_dest = strlen(dest);
 	int len_src = strlen(src);
 	int str_size = len_dest + len_src + 1;
-	char *a = calloc(str_size, sizeof(char));
+	char *a = calloc(str_size + 1, sizeof(char));
 
 	for (int i = 0; i < len_dest; i++)
 		a[i] = dest[i];
+
+	a[len_dest] = ' ';
+	
 	for (int i = 0; i < len_src; i++)
 		a[len_dest + i] = src[i];
+	
 	a[str_size - 1] = '\0';
 	_putchar('\n');
 	return (a);
