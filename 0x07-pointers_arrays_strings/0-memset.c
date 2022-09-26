643bf7f58f2fb2e44c47aaf83a7e7d8037027920
barasamichael <ourdigitaltimes@gmail.com>
2022-09-26 22:46:33 +0300

    Mega Commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,6 @@
 
 /**
  * _memset - fills first n bytes of memory area pointed to by
- * s with constant byte b
  * @s: a pointer to a memory area.
  * @b:the constant byte.
  * @n: the number of bytes of memory to be filled with b.
